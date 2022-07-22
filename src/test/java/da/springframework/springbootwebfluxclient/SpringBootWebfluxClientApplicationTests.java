cc0f2262315cddd0cde4da2414cba3f52090d652
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-22 12:32:52 -0500

    88. Creating microservice client. Initial Commit

---
+++ src/test/java/da/springframework/springbootwebfluxclient/SpringBootWebfluxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.springbootwebfluxclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
