3259865579c092c4db66ac6e036f14753d5890a2
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-22 13:12:43 -0500

    91. Implementing CRUD on Service. Closes #3

--- src/main/java/da/springframework/springbootwebfluxclient/services/ProductServiceImpl.java
+++ src/main/java/da/springframework/springbootwebfluxclient/services/ProductServiceImpl.java
@@ -2,15 +2,20 @@ package da.springframework.springbootwebfluxclient.services;
 
 import da.springframework.springbootwebfluxclient.model.ProductDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
 @RequiredArgsConstructor
+@Service
 public class ProductServiceImpl implements ProductService {
 
     private WebClient webClient;
@@ -18,7 +23,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Flux<ProductDTO> findAll() {
         return webClient.get()
-                .accept(MediaType.APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
                 .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(ProductDTO.class));
 //                .retrieve().bodyToFlux(ProductDTO.class); // same as above
     }
@@ -30,7 +35,7 @@ public class ProductServiceImpl implements ProductService {
         params.put("id", id);
 
         return webClient.get().uri("/{id}", params)
-                .accept(MediaType.APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
                 .retrieve().bodyToMono(ProductDTO.class);
 //                .exchangeToMono(clientResponse -> clientResponse.bodyToMono(ProductDTO.class)); // same as above
 
@@ -38,16 +43,26 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Mono<ProductDTO> save(ProductDTO productDTO) {
-        return null;
+        return webClient.post()
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(productDTO))
+                .retrieve().bodyToMono(ProductDTO.class);
     }
 
     @Override
     public Mono<ProductDTO> update(ProductDTO productDTO, String id) {
-        return null;
+
+        return webClient.put().uri("/{id}", Collections.singletonMap("id", id))
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(productDTO))
+                .retrieve().bodyToMono(ProductDTO.class);
     }
 
     @Override
     public Mono<Void> delete(String id) {
-        return null;
+        return webClient.delete().uri("/{id}", Collections.singletonMap("id", id))
+                .retrieve().toBodilessEntity().then(); //exchange().then()
     }
 }
